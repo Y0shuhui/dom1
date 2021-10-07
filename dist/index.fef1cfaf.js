a3b6a3738aae5fa0a83c8ddba35c3539ed4e62e7
sallyyuan <1319773277@qq.com>
2021-10-07 15:32:52 +0800

    修改了 JS

--- dist/index.fef1cfaf.js
+++ dist/index.fef1cfaf.js
@@ -456,10 +456,8 @@ function hmrAcceptRun(bundle, id) {
 
 },{}]},["3QDhh"], null, "parcelRequire94c2")
 const div = dom.find('#test>.red')[0];
-console.log(div);
 dom.style(div, 'color', 'red');
 const div1 = dom.find('#test>.blue')[0];
-console.log(div1);
 dom.style(div1, 'color', 'blue');
 const divList = dom.find('.red1');
 dom.each(divList, (n)=>console.log(n)
--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 const div = dom.find('#test>.red')[0] 
-console.log(div)
 dom.style(div, 'color', 'red') 
 
 const div1 = dom.find('#test>.blue')[0] 
-console.log(div1)
 dom.style(div1, 'color', 'blue')
 
 const divList = dom.find('.red1')
